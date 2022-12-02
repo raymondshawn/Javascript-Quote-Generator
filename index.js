6a329d5d4a9dca1ca0a7785071e09f699dbf0c54
raymond shawn <76945071+raymondshawn@users.noreply.github.com>
2022-12-02 12:53:27 -0500

    used math random

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
-
-const data=()=>{
-
-    const response=fetch("https://type.fit/api/quotes")
-    .then(function(response) {
+const data = () => {
+  const response = fetch("https://type.fit/api/quotes")
+    .then(function (response) {
       return response.json();
     })
-    .then(function(data) {
-     
-      return data
+    .then(function (data) {
+      return data;
     });
-    return response
-}
+  return response;
+};
 
+let quotes;
 
+data()
+  .then((response) => {
+    console.log("first then response");
+    quotes = response;
+    return response;
+  })
+  .then(() => {
+    console.log("second then finished");
+  });
 
+setTimeout(() => {
+  const author = document.getElementById("author");
+  const quote = document.getElementById("quote");
+  const btn = document.getElementById("btn");
 
-let quotes; 
- 
- data().then(response=>{
-   
-    console.log('first then response')
-    quotes=response
-    return response
-    
- }).then(()=>{
-    
-    console.log('second then finished');
- })
-   
-
-setTimeout(()=>{
-
-    
-
-},1000) 
-  
- 
+  btn.addEventListener("click", () => {
+    let randomNumber = Math.floor(Math.random() * quotes.length);
+    const text = quotes[randomNumber].text;
+    const auth = quotes[randomNumber].author;
+    quote.textContent = text;
+    author.textContent = auth;
+  });
+}, 1000);
